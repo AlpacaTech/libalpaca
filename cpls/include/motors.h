cd8d13ebf494e94fcef62cb7f80f8063c13a8dfc
iuyte <iuyte@gmail.com>
2017-05-07 21:12:31 -0400

    Re-added slew functionality, hopeful of success. Also, added .gitignore file

--- cpls/include/motors.h
+++ cpls/include/motors.h
@@ -4,8 +4,9 @@ struct motor {
   unsigned char port;
   char inverted;
   int power;
-  long slewRate;
+  unsigned long slewRate;
   float scale;
+  unsigned long tlast;
   void set(int power);
 };
 
@@ -13,4 +14,9 @@ namespace motors {
   void set(motor _motor, int power);
   int get(motor _motor);
   motor init(unsigned char port, int inverted, long slewRate, float scale);
+  namespace slew {
+    extern motor list[11];
+    extern TaskHandle handle;
+    void init(void);
+  }
 }
--- cpls/src/motors.cpp
+++ cpls/src/motors.cpp
@@ -1,7 +1,8 @@
 #include "motors.h"
 
-void motor::set(int power) {
-  motorSet(port, power * inverted * scale);
+void motor::set(int _power) {
+  power = _power * inverted * scale;
+  motors::slew::list[port].power = power;
 }
 
 namespace motors {
@@ -19,6 +20,38 @@ namespace motors {
     _motor.inverted = inverted;
     _motor.slewRate = slewRate;
     _motor.scale = scale;
+    slew::list[_motor.port] = _motor;
     return _motor;
+  }
+
+  namespace slew {
+    motor list[11];
+    TaskHandle handle;
+
+    void _slew(void* none) {
+      unsigned long int current;
+      while (true) {
+        current = millis();
+        for (size_t i = 1; i <= 11; i++) {
+          if (current - list[i].tlast < list[i].slewRate) {
+            continue;
+          }
+          motorSet(list[i].port, list[i].power);
+          list[i].tlast = current;
+        }
+      }
+    }
+
+    void init(void) {
+      motor default_motor;
+      default_motor.inverted = 1;
+      default_motor.slewRate = 1;
+      default_motor.scale = 0;
+      for (size_t i = 1; i <= 11; i++) {
+        default_motor.port = i;
+        list[i] = default_motor;
+      }
+      handle = taskCreate(&_slew, TASK_DEFAULT_STACK_SIZE, NULL, TASK_PRIORITY_DEFAULT + 1);
+    }
   }
 }
