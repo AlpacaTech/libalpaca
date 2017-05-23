91666d0d582b3f1c99d5b9a303c94064a114df61
iuyte <iuyte@gmail.com>
2017-05-23 01:01:40 -0400

    More sensor classes

--- cpls/include/sensors.hpp
+++ cpls/include/sensors.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <API.h>
+#include <vector>
 
 namespace sensors {
 struct quad_t {
@@ -10,9 +11,45 @@ struct quad_t {
   void reset(void);
   long value(void);
   float request;
+  void init(void);
+  quad_t(unsigned char port1, unsigned char port2, bool _inverted);
 };
-extern quad_t left;
-extern quad_t right;
+class gyro_t {
+  int calibration;
+
+public:
+  Gyro gyro;
+  unsigned char port;
+  long zero;
+  void reset(void);
+  long value(void);
+  float request;
+  void init(void);
+  gyro_t(unsigned char _port, unsigned int _calibration);
+};
+struct pot_t {
+  unsigned char port;
+  long zero;
+  bool inverted;
+  void reset(void);
+  long value(void);
+  float request;
+  void init(void);
+  pot_t(unsigned char _port, bool _inverted);
+};
+struct button_t {
+  unsigned char port;
+  bool inverted;
+  bool value(void);
+  void init(void);
+  button_t(unsigned char _port, bool _inverted);
+};
 void init(void);
 void reset(void);
+
+extern quad_t left;
+extern quad_t right;
+extern gyro_t gyro;
+extern pot_t pot;
+extern button_t button;
 }
--- cpls/src/sensors.cpp
+++ cpls/src/sensors.cpp
@@ -2,30 +2,78 @@
 
 namespace sensors {
 
-quad_t left;
-quad_t right;
+quad_t left(1, 2, false);
+quad_t right(3, 4, false);
+gyro_t gyro(1, 198);
+pot_t pot(2, false);
+button_t button(5, false);
 
-void quad_t::reset(void) { encoderReset(quad_t::enc); }
+quad_t::quad_t(unsigned char port1, unsigned char port2, bool _inverted) {
+  ports[0] = port1;
+  ports[1] = port2;
+  inverted = _inverted;
+  zero = 0;
+  request = 0;
+}
+void quad_t::init(void) {
+  enc = encoderInit(quad_t::ports[0], quad_t::ports[1], quad_t::inverted);
+}
+long quad_t::value(void) { return (encoderGet(enc) - zero); }
+void quad_t::reset(void) {
+  zero = encoderGet(enc);
+  request = 0;
+}
 
-long quad_t::value(void) { return (encoderGet(quad_t::enc) - quad_t::zero); }
+gyro_t::gyro_t(unsigned char _port, unsigned int _calibration) {
+  port = _port;
+  calibration = _calibration;
+  zero = 0;
+  request = 0;
+}
+void gyro_t::init(void) { gyro_t::gyro = gyroInit(port, calibration); }
+long gyro_t::value(void) { return (gyroGet(gyro_t::gyro) - zero); }
+void gyro_t::reset(void) {
+  zero = gyroGet(gyro_t::gyro);
+  request = 0;
+}
+
+pot_t::pot_t(unsigned char _port, bool _inverted) {
+  port = _port;
+  inverted = _inverted;
+  zero = 0;
+  request = 0;
+}
+void pot_t::init(void) { analogCalibrate(port); }
+long pot_t::value(void) {
+  return ((analogReadCalibrated(port) - zero) * (inverted) ? -1 : 1);
+}
+void pot_t::reset(void) {
+  zero = analogReadCalibrated(port);
+  request = 0;
+}
+
+button_t::button_t(unsigned char _port, bool _inverted) {
+  port = _port;
+  inverted = _inverted;
+}
+void button_t::init(void) { pinMode(port, INPUT); }
+bool button_t::value(void) {
+  return (digitalRead(port)) ? ((inverted) ? false : true)
+                             : ((inverted) ? true : false);
+}
 
 void init(void) {
-  left.ports[0] = 1;
-  left.ports[1] = 2;
-  right.ports[0] = 3;
-  right.ports[1] = 4;
-  left.inverted = false;
-  right.inverted = false;
-  left.enc = encoderInit(left.ports[0], left.ports[1], left.inverted);
-  right.enc = encoderInit(right.ports[0], right.ports[1], right.inverted);
-  left.zero = 0;
-  right.zero = 0;
+  left.init();
+  right.init();
+  gyro.init();
+  pot.init();
+  button.init();
 }
 
 void reset(void) {
   left.reset();
   right.reset();
-  left.request = 0;
-  right.request = 0;
+  gyro.reset();
+  pot.reset();
 }
 }
