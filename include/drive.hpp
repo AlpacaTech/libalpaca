25545ec9c215f06b15941218473a8fa3cbcec1f5
iuyte <iuyte@gmail.com>
2017-06-06 17:28:54 -0400

    added file boilerplates

--- include/drive.hpp
+++ include/drive.hpp
@@ -1,3 +1,19 @@
+// drive.hpp: Header file for utilities relating to the drive
+// Copyright (C) 2017 Ethan Wells
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #pragma once
 #include "motors.hpp"
 #include "sensors.hpp"
@@ -13,21 +29,26 @@ struct side_t {
 extern double inch;
 extern side_t left;
 extern side_t right;
-/* Set both sides of the drive at their requested powers
- */
+
+/** Set both sides of the drive at their requested powers */
 void set(int lpower, int rpower);
+
+/** Initialize the drive system */
 void init(void);
-/* Tank control that can (and should) be in a while loop
- */
+
+/** Drive a specific number of inches */
+void inches(long inches);
+
+/** Tank control that should be used in a while loop */
 void tank(void);
 namespace accel {
 extern int x;
 extern int y;
 extern int prevX;
 extern int prevY;
+
+/** Tilt control using the josytick accelerometer. Should be used in a while
+ * loop */
 void drive(void);
 }
-/* Drive a specific number of inches
- */
-void inches(long inches);
 }
--- src/drive.cpp
+++ src/drive.cpp
@@ -1,3 +1,19 @@
+// drive.cpp: Source file for utilities relating to the drive
+// Copyright (C) 2017 Ethan Wells
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "../include/main.h"
 
 namespace drive {
@@ -22,15 +38,11 @@ void init(void) {
   right.sensor = &sensors::right;
 }
 
-/* Set both sides of the drive at their requested powers
- */
 void set(int lpower, int rpower) {
   left.set(lpower);
   right.set(rpower);
 }
 
-/* Tank control that can (and should) be in a while loop
- */
 void tank(void) {
   int deadband = 20;
   int lj       = joystickGetAnalog(1, 3);
@@ -88,8 +100,6 @@ void drive(void) {
 }
 }
 
-/* Drive a specific number of inches
- */
 void inches(long inches) {
   pid::enable();
   left.sensor->request += inches * inch;
