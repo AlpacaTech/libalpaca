8f91b102fc55ab79733313c33e7415ef5419aa5e
Ethan Wells <iuyte@gmx.com>
2017-06-29 09:39:59 -0400

    Changed .gitignore

--- vim.sh
+++
@@ -1,2 +0,0 @@
-cd /home/ethan/VEX-709S-2018/
-gvim include/motors.hpp src/motors.cpp include/sensors.hpp src/sensors.cpp include/drive.hpp src/drive.cpp include/pid.hpp src/pid.cpp include/lift.hpp src/lift.cpp include/gyro.hpp src/gyro.cpp src/init.cpp src/auto.cpp src/opcontrol.cpp
