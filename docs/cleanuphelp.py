967d059d54880965daacbe4bcfae10ba66ba10d7
Ethan Wells <iuyte@gmx.com>
2017-07-05 20:13:13 -0400

    Small changes to documentation scripts

--- docs/cleanuphelp.py
+++ docs/cleanuphelp.py
@@ -1,14 +1,15 @@
-import sys, jsonpickle
+import sys
+import jsonpickle
 from commit import Commit, printCommit, saveList
 
 message = \
-"""
+    """
 """
 
 year = "2018"
 
 style = \
-"""+++
+    """+++
 date = "2017-06-18T16:20:34-04:00"
 description = "A log off all that was programmed"
 title = "Programming Engineering Notebook"
@@ -25,7 +26,7 @@ with open("main.js", mode="r") as scriptfile:
     script = scriptfile.read()
 
 commitTemp =\
-"""
+    """
 <div class='commit' id='{1}'>
 <p><b style='cursor: pointer;' onclick='window.open(\"https://github.com/iuyte/VEX-709S-{2}/commit/{3}\")'>Commit:</b> <a class='link' href='https://iuyte.github.io/VEX-709S-{2}#{3}'>{3}</a></p>
 <p><b>Date:</b> {4}</p>
@@ -48,19 +49,27 @@ def indexInStr(index, string):
 
 def styleContent(commit):
     out = "<div class='commit' id='" + commit.commitkey + "'>"
-    out += "<p><b><a class='link' href='https://github.com/iuyte/VEX-709S-" + year + "/commit/" + commit.commitkey + "/' target='_blank'>Commit:</a></b> <a class='link' href='https://iuyte.github.io/notebook/programming/#" + commit.commitkey + "'>" + commit.commitkey + "</a></p>"
+    out += "<p><b><a class='link' href='https://github.com/iuyte/VEX-709S-" + year + "/commit/" + commit.commitkey + \
+        "/' target='_blank'>Commit:</a></b> <a class='link' href='https://iuyte.github.io/notebook/programming/#" + \
+        commit.commitkey + "'>" + commit.commitkey + "</a></p>"
     out += "<p><b>Date:</b> " + commit.date + "</p>\n"
     out += "<p><b>Author:</b> " + commit.author + "</p>\n"
     out += "<p><b>Description:</b><br>" + commit.description + "</p>\n"
     if len(commit.filesAdded) > 0:
         out += "<b>Files added:</b>\n<ul>\n"
         for i in range(len(commit.filesAdded)):
-            out += "<li><a class='link' href='https://github.com/iuyte/VEX-709S-" + year + "/tree/" + commit.commitkey + "/" + commit.filesAdded[i] + "'>" + commit.filesAdded[i] + "</a></li>\n"
+            out += "<li><a class='link' href='https://github.com/iuyte/VEX-709S-" + year + "/tree/" + \
+                commit.commitkey + "/" + \
+                commit.filesAdded[i] + "'>" + \
+                commit.filesAdded[i] + "</a></li>\n"
         out += "</ul>"
     if len(commit.filesModified) > 0:
         out += "<b>Files modified:</b>\n<ul>\n"
         for i in range(len(commit.filesModified)):
-            out += "<li><a class='link' href='https://github.com/iuyte/VEX-709S-" + year + "/tree/" + commit.commitkey + "/" + commit.filesModified[i] + "'>" + commit.filesModified[i] + "</a></li>\n"
+            out += "<li><a class='link' href='https://github.com/iuyte/VEX-709S-" + year + "/tree/" + \
+                commit.commitkey + "/" + \
+                commit.filesModified[i] + "'>" + \
+                commit.filesModified[i] + "</a></li>\n"
         out += "</ul>"
     if len(commit.filesDeleted) > 0:
         out += "<b>Files deleted:</b>\n<ul>\n"
@@ -121,11 +130,11 @@ def parseContent(content):
         for aline in range(aline, len(lines)):
             curline = lines[aline][39:]
             if ". M" in lines[aline]:
-                if "docs/index.html" not in curline and "docs/log.txt" not in curline and "docs/log.json" not in curline and "bin/" not in curline and "__pycache__" not in curline and "tools/doxygen" not in curline and "tools/PDFs" not in curline and "tools/LATEX" not in curline and not curline.startswith("doxygen"):
+                if "docs/index.html" not in curline and "docs/log.txt" not in curline and "docs/log.json" not in curline and "bin/" not in curline and "__pycache__" not in curline and "tools/doxygen" not in curline and "tools/PDFs" not in curline and "tools/LATEX" not in curline and not curline.startswith("doxygen") and not curline.startswith("709S"):
                     filesModified.append(curline)
-            elif ". A" in lines[aline] and ".swp" not in curline and "bin/" not in curline and "__pycache__" not in curline and "tools/doxygen" not in curline and "tools/PDFs" not in curline and "tools/LATEX" not in curline and not curline.startswith("doxygen"):
+            elif ". A" in lines[aline] and ".swp" not in curline and "bin/" not in curline and "__pycache__" not in curline and "tools/doxygen" not in curline and "tools/PDFs" not in curline and "tools/LATEX" not in curline and not curline.startswith("doxygen") and not curline.startswith("709S"):
                 filesAdded.append(curline)
-            elif ". D" in lines[aline] and ".swp" not in curline and "bin/" not in curline and "__pycache__" not in curline and "tools/doxygen" not in curline and "tools/PDFs" not in curline and "tools/LATEX" not in curline and not curline.startswith("doxygen"):
+            elif ". D" in lines[aline] and ".swp" not in curline and "bin/" not in curline and "__pycache__" not in curline and "tools/doxygen" not in curline and "tools/PDFs" not in curline and "tools/LATEX" not in curline and not curline.startswith("doxygen") and not curline.startswith("709S"):
                 filesDeleted.append(curline)
         tcommit = Commit(date, commitkey, author, description, filesModified,
                          filesAdded, filesDeleted)
